4e57c22a7ae5835b27790c9ee3b9f30cbccb868f
Simakov Nikita <simakovnikita4@gmail.com>
2022-03-27 21:20:55 +0600

    task

---
+++ src/test/java/FormTests.java
@@ -0,0 +1,67 @@
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import pages.form.FormPage;
+import pages.form.ModalWindow;
+import utils.Attachments;
+
+import java.util.Locale;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FormTests extends TestBase{
+
+    final Faker faker = new Faker(new Locale("ru"));
+
+    final String name = faker.pokemon().name();
+    final String surname = faker.gameOfThrones().house();
+    final String email = faker.internet().emailAddress();
+    final String genderRadioNumber = "3";
+    final String phoneNumber = String.valueOf(faker.number().numberBetween(1111111111L, 9999999999L));
+    final String day = "10";
+    final String month = "May";
+    final String year = "1978";
+    final String subjectsInput = "Hist";
+    final String hobbieCheckboxNumber = "2";
+    final String fileName = "rickroll.jpg";
+    final String currentAddress = faker.address().streetAddress();
+    final String state = "NCR";
+    final String city = "Delhi";
+
+    @Test
+    void fillForm() {
+        open("https://demoqa.com/automation-practice-form");
+        final FormPage formPage = new FormPage();
+
+        formPage.setFirstName(name)
+                .setLastName(surname)
+                .setEmail(email)
+                .clickGenderRadioWithNumber(genderRadioNumber)
+                .setPhoneNumber(phoneNumber)
+                .setDate(day, month, year)
+                .setSubjects(subjectsInput)
+                .clickHobbieCheckbox(hobbieCheckboxNumber)
+                .uploadPicture(fileName)
+                .setAddress(currentAddress)
+                .setState(state)
+                .setCity(city)
+                .submit();
+
+        final ModalWindow modalWindow = new ModalWindow();
+        modalWindow
+                .checkResult(name + " " + surname)
+                .checkResult(email)
+                .checkResult("Other")
+                .checkResult(phoneNumber)
+                .checkResult(day + " " + month + "," + year)
+                .checkResult("History")
+                .checkResult("Reading")
+                .checkResult(fileName)
+                .checkResult(currentAddress)
+                .checkResult(state + " " + city)
+                .closeModal();
+    }
+}
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,35 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import utils.Attachments;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attachments.pageSource();
+        Attachments.addVideo();
+        Attachments.screenshotAs("Screenshot after test");
+        Attachments.browserConsoleLogs();
+        closeWebDriver();
+    }
+}

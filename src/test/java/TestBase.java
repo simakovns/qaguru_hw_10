e9d94b9e6b0202b822f40b662a943bb498685b49
Nikita <64635044+simakovns@users.noreply.github.com>
2022-04-06 13:22:54 +0600

    Update TestBase.java

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -17,8 +17,6 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.browser = "Chrome";
-        Configuration.browserVersion = "91";
-
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
